179afeb67f97d91938cfe8a3d36c40a52d8d9fc3
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-26 18:21:37 +1100

    first-commit

---
+++ class-1/function.js
@@ -0,0 +1,21 @@
+function evenodd(num){
+    if(num % 2 == 0){
+        console.log(num,"is a even number")
+    }
+    else{
+        console.log(num,'is a odd number')
+    }
+}
+evenodd(10)
+evenodd(11)
+
+//array value double
+function array(name){
+var name =[23,43,45,67,43]
+
+    for(var i = 0;i<name.length;i++){
+        var element = name[i];
+    console.log(element)
+    }
+}
+array()
